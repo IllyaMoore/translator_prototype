c9c8edf56959e76e632caa4c6793af3460381e2c
moore <illyaipie@gmail.com>
2025-10-07 15:59:23 +0300

    timing measuring

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import subprocess
-
 import os
-
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
+
+import time 
+_test_timer = time.time()
 
 CAPTURE_VOICE = os.getenv("CAPTURE_VOICE")
 
@@ -18,8 +19,12 @@ TTS = os.getenv("TTS")
 # subprocess.run(["python3", CAPTURE_VOICE])
 print("\n")
 subprocess.run(["python3", GET_SPEECH_TXT])
-
+print("\n")
 subprocess.run(["python3", TRANSLATE])
-
+print("\n")
 subprocess.run(["python3", TTS])
+print("\n")
+print(f"Time for execution {format(time.time() - _test_timer, '.0f')} sec")
+print("\n")
+
 
